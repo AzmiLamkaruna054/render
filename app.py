242eb7e06014e0af426f8f264c590d5f2bc24d75
AzmiLamkaruna054 <167405723+AzmiLamkaruna054@users.noreply.github.com>
2025-02-22 11:14:17 +0700

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, jsonify
+import requests
+import os
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
-app = Flask(__name__)
+MODEL_URL = "https://www.dropbox.com/scl/fi/zare7lltsj94cwjlmb6z9/PISANG16CLASS.h5?rlkey=5ohf9ddgxo1j8753tzx8igtim&st=9pi4643l&dl=1"
+MODEL_PATH = "models/PISANG16CLASS.h5"
 
-@app.route('/')
-def home():
-    return jsonify({"message": "API is running!"})
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Dropbox...")
+    os.makedirs("models", exist_ok=True)
+    response = requests.get(MODEL_URL)
+    with open(MODEL_PATH, "wb") as f:
+        f.write(response.content)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Load model
+model = load_model(MODEL_PATH)
+
